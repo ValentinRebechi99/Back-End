2f21b2edfbbe86ca32e77ab17fef98820b270b09
ValentinRebechi99 <71794802+ValentinRebechi99@users.noreply.github.com>
2022-10-12 10:03:55 -0300

    Se agregan Archvios del Back end

---
+++ BackEnd/ValentinRebechiBackEnd/src/main/java/com/Portfolio/ValentinRebechi/ValentinRebechiApplication.java
@@ -0,0 +1,13 @@
+package com.Portfolio.ValentinRebechi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValentinRebechiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValentinRebechiApplication.class, args);
+    }
+
+}
---
+++ BackEnd/ValentinRebechiBackEnd/src/test/java/com/Portfolio/ValentinRebechi/ValentinRebechiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Portfolio.ValentinRebechi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValentinRebechiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
