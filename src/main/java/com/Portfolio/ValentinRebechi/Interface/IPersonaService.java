122807f8015433c2ba051e22c8eb1de0da338ea2
ValentinRebechi99 <valenrebechi64@gmail.com>
2022-10-26 09:33:51 -0300

    Modificacion

---
+++ src/main/java/com/Portfolio/ValentinRebechi/Interface/IPersonaService.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.Portfolio.ValentinRebechi.Interface;
+
+import com.Portfolio.ValentinRebechi.Entidad.Persona;
+import java.util.List;
+
+/**
+ *
+ * @author vale_
+ */
+public interface IPersonaService {
+    //Traer una lista de personas
+    public List<Persona> getPersona();
+    
+    //Guardar un objeto de tipo Persona
+    public void savePersona(Persona persona);
+    
+    //Eliminar un objeto pero lo buscamos por ID
+    public void deletePersona(Long id);
+    
+    //Buscar una persona por ID
+    public Persona findPersona(Long id);
+}
---
+++ src/main/java/com/Portfolio/ValentinRebechi/Security/Service/RolService.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Portfolio.ValentinRebechi.Security.Service;
+
+import com.Portfolio.ValentinRebechi.Security.Entity.Rol;
+import com.Portfolio.ValentinRebechi.Security.Enums.RolNombre;
+import com.Portfolio.ValentinRebechi.Security.Repository.iRolRepository;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author vale_
+ */
+@Service
+@Transactional
+public class RolService {
+    @Autowired
+    iRolRepository irolRepository;
+    
+    public Optional<Rol>getByRolNombre(RolNombre rolNombre){
+       return irolRepository.findByRolNombre(rolNombre);
+    }
+public void save(Rol rol){
+    irolRepository.save(rol);
+}
+}
